0f7c49ba2e61b405fb2006fe258867ae1abf5a43
Kevin Waldock <kevin.waldock@gmail.com>
2016-11-16 13:47:55 +0100

    Replace std::function with template arguments

--- test/TestMain.cpp
+++ test/TestMain.cpp
@@ -13,5 +13,43 @@ TEST_CASE("Main Test", "[csv-reader]") {
 
     csv_reader
         .ReadDataLine(&test_string, &answer, &five)
-        .ReadDataLine(CSVReader::CSVDiscard(), &world_string);
+        .ReadDataLine(CSVReader::CSVDiscard(), CSVReader::MakeCSVValidator(&world_string, nullptr));
 }
+
+TEST_CASE("Test CSVDiscard", "[csv-reader]") {
+    auto reader = CSVReader::MakeStringReader(std::string("This is a test,42"));
+    auto csv_reader = CSVReader::MakeCSVReaderFromBasicString(&reader, ',');
+
+    int num;
+    csv_reader.ReadDataLine(CSVReader::CSVDiscard(), &num);
+    REQUIRE(num == 42);
+}
+
+TEST_CASE("Test CSVValidator", "[csv-reader]") {
+    auto reader = CSVReader::MakeStringReader(std::string("This is a test,42"));
+    auto csv_reader = CSVReader::MakeCSVReaderFromBasicString(&reader, ',');
+
+    int num;
+    REQUIRE_NOTHROW(csv_reader.ReadDataLine(CSVReader::CSVDiscard(), CSVReader::MakeCSVValidator(&num, [](int* val) {return *val == 42; })));
+}
+
+TEST_CASE("Test PostProcessor", "[csv-reader]") {
+    auto reader = CSVReader::MakeStringReader(std::string("This is a test,42"));
+    auto csv_reader = CSVReader::MakeCSVReaderFromBasicString(&reader, ',');
+
+    int num;
+    csv_reader.ReadDataLine(CSVReader::CSVDiscard(), CSVReader::MakeCSVPostProcessor(&num, [](int& val) {val /= 2; }));
+    REQUIRE(num == 21);
+}
+
+TEST_CASE("Test Optional", "[csv-reader]") {
+    auto reader = CSVReader::MakeStringReader(std::string("This is a test"));
+    auto csv_reader = CSVReader::MakeCSVReaderFromBasicString(&reader, ',');
+
+    int num;
+    csv_reader.ReadDataLine(CSVReader::CSVDiscard(), CSVReader::MakeCSVOptional(&num, 21));
+    REQUIRE(num == 21);
+}
+
+
+
